a181966b202b4e4c423add18e32b82419ea0f4b7
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-23 11:20:42 +0330

    The Users app added to the project.

--- backend/Server/Server/settings.py
+++ backend/Server/Server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom apps
+    'Users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
