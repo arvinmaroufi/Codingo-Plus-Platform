3f4bd0d498033fa7f8cd4c2b2d60a2dd273a8d4a
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-29 17:32:42 +0330

    The urls are modified.

--- backend/Server/Server/urls.py
+++ backend/Server/Server/urls.py
@@ -5,7 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('courses/', include('Courses.urls')),
     path('courses/', include('Courses.urls', namespace="Courses")),
-    path('profiles/', include('Courses.urls', namespace="Profiles")),
+    path('profiles/', include('Profiles.urls', namespace="Profiles")),
 ]
