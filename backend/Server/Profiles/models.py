a2a1d5a13451da11f595391ac61ae6a8445ab39f
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-30 12:42:44 +0330

    The StudentProfile models is added and migrated to the db.

--- backend/Server/Profiles/models.py
+++ backend/Server/Profiles/models.py
@@ -12,7 +12,7 @@ class TeacherProfile(models.Model):
     user = models.OneToOneField(
         "Users.User",
         on_delete=models.CASCADE,
-        related_name="provider_profile",
+        related_name="teacher_profile",
         verbose_name="کاربر"
     )
 
@@ -68,3 +68,69 @@ class TeacherProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - پروفایل استاد"
+
+
+class StudentProfile(models.Model):
+    class GenderChoices(models.TextChoices):
+        WOMEN = 'W', 'خانوم'
+        MAN = 'M', 'آقا'
+
+    user = models.OneToOneField(
+        "Users.User",
+        on_delete=models.CASCADE,
+        related_name="student_profile",
+        verbose_name="کاربر"
+    )
+
+    gender = models.CharField(
+        max_length=1,
+        choices=GenderChoices.choices,
+        verbose_name="جنسیت"
+    )
+
+    age = models.PositiveIntegerField(
+        default=18,
+        verbose_name="سن"
+    )
+
+    bio = RichTextField(
+        verbose_name="بیو",
+        blank=True,
+        null=True,
+    )
+
+    profile_picture = models.ImageField(
+        upload_to='Profiles/students/picture/',
+        null=True,
+        blank=True,
+        verbose_name="تصویر پروفایل"
+    )
+
+    profile_banner = models.ImageField(
+        upload_to='Profiles/students/banner/',
+        null=True,
+        blank=True,
+        verbose_name="بنر پروفایل"
+    )
+
+    is_available = models.BooleanField(
+        default=True,
+        verbose_name="در دسترس بودن"
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="تاریخ ایجاد"
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="تاریخ به‌روزرسانی"
+    )
+
+    class Meta:
+        verbose_name = "پروفایل دانشجو"
+        verbose_name_plural = "پروفایل دانشجویان"
+
+    def __str__(self):
+        return f"{self.user.username} - پروفایل دانشجو"
