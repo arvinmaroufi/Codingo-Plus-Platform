03669e4c411467aec7aac1e3e51b44915fb9ce2a
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-30 12:39:54 +0330

    The TeacherProfile model is added and migrated to the db.

--- backend/Server/Profiles/models.py
+++ backend/Server/Profiles/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+
+class TeacherProfile(models.Model):
+
+    class GenderChoices(models.TextChoices):
+        WOMEN = 'W', 'خانوم'
+        MAN = 'M', 'آقا'
+
+    user = models.OneToOneField(
+        "Users.User",
+        on_delete=models.CASCADE,
+        related_name="provider_profile",
+        verbose_name="کاربر"
+    )
+
+    gender = models.CharField(
+        max_length=1,
+        choices=GenderChoices.choices,
+        verbose_name="جنسیت"
+    )
+
+    age = models.PositiveIntegerField(
+        default=18,
+        verbose_name="سن"
+    )
+
+    bio = RichTextField(
+        verbose_name="بیو",
+        blank=True,
+        null=True,
+    )
+
+    profile_picture = models.ImageField(
+        upload_to='Profiles/teachers/picture/',
+        null=True,
+        blank=True,
+        verbose_name="تصویر پروفایل"
+    )
+
+    profile_banner = models.ImageField(
+        upload_to='Profiles/teachers/banner/',
+        null=True,
+        blank=True,
+        verbose_name="بنر پروفایل"
+    )
+
+    is_available = models.BooleanField(
+        default=True,
+        verbose_name="در دسترس بودن"
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="تاریخ ایجاد"
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="تاریخ به‌روزرسانی"
+    )
+
+    class Meta:
+        verbose_name = "پروفایل استاد"
+        verbose_name_plural = "پروفایل اساتید"
+
+    def __str__(self):
+        return f"{self.user.username} - پروفایل استاد"
