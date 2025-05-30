9edd17d4e3105e386a401feee1c62e4b837c851f
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-05-30 13:19:14 +0330

    The dark and light color pallet is added to the tailwind css config file.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -12,7 +12,23 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Dark theme colors
+        'base-dark': '#031525',
+        'primary-dark': '#0794B0',
+        'secondary-dark': '#20CCC7',
+        'main-text-dark': '#DAEBE7',
+        'highlight-text-dark': '#E1F0F7',
+        // Light theme colors
+        'base-light': '#EAF1F0',
+        'primary-light': '#63C9DD',
+        'secondary-light': '#AAE4E2',
+        'main-text-light': '#04364C',
+        'highlight-text-light': '#DAEBE7',
+
+      },
+    },
   },
   plugins: [],
 }
