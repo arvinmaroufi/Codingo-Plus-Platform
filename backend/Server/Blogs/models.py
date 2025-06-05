84321f5287f4bba50c2e42098ff848ca71a25bbf
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-05 05:11:59 +0430

    SubCategory is added to the models and migrated to the db

--- backend/Server/Blogs/models.py
+++ backend/Server/Blogs/models.py
@@ -16,3 +16,21 @@ class MainCategory(models.Model):
     def __str__(self):
         return self.title
     
+
+class SubCategory(models.Model):
+    """Represents subcategories under a main category."""
+    parent = models.ForeignKey(MainCategory, on_delete=models.CASCADE, related_name='subcategories', verbose_name='دسته بندی والد')  # Links to a main category
+    title = models.CharField(max_length=100, unique=True, verbose_name='عنوان دسته بندی')  # Subcategory title, ensuring uniqueness
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='نامک')  # URL-friendly identifier
+    icon = models.FileField(upload_to='Blogs/SubCategory_icons/')  # Subcategory icon
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')  
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ بروزرسانی')
+
+    class Meta:
+        verbose_name = 'زیر دسته بندی'
+        verbose_name_plural = 'زیر دسته بندی ها'
+
+    def __str__(self):
+        return self.title
+    
+
