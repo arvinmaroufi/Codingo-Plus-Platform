176a9ac73e5db3d31b780d6925d7bc6858419e40
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-05 05:16:40 +0430

    Tag is added to the models and migrated to the db

--- backend/Server/Blogs/models.py
+++ backend/Server/Blogs/models.py
@@ -24,7 +24,7 @@ class SubCategory(models.Model):
     slug = models.SlugField(max_length=100, unique=True, verbose_name='نامک')  # URL-friendly identifier
     icon = models.FileField(upload_to='Blogs/SubCategory_icons/')  # Subcategory icon
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')  
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ بروزرسانی')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ به‌روزرسانی')
 
     class Meta:
         verbose_name = 'زیر دسته بندی'
@@ -33,4 +33,19 @@ class SubCategory(models.Model):
     def __str__(self):
         return self.title
     
+
+class Tag(models.Model):
+    """Represents tags that can be associated with blog posts for categorization."""
+    title = models.CharField(max_length=100, unique=True, verbose_name='عنوان برچسب')  # Tag name, ensuring uniqueness
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='نامک')  # URL-friendly identifier for tags
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')  # Timestamp for when the tag was created
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ به‌روزرسانی')  # Automatically updates when modified
+
+    class Meta:
+        verbose_name = 'برچسب'
+        verbose_name_plural = 'برچسب ها'
+
+    def __str__(self):
+        return self.title
+
 
