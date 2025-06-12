73183a6ea00e7ead1e15f28dc54fa93223647237
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-12 16:30:44 +0330

    The urls and auth login password url is added to the project.

--- backend/Server/Server/urls.py
+++ backend/Server/Server/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('courses/', include('Courses.urls')),
     path('tickets/', include('Tickets.urls')),
     path('blogs/', include('Blogs.urls')),
+    path('auth/', include('Authentication.urls', namespace="authentication")),
     # ckeditor_editor url
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
