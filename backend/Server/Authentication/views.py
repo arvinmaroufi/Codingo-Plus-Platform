559318db83fa1f010854e79f430a502df951b6ff
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-12 16:30:10 +0330

    The LoginPasswordAPIView is added to the views.py file.

--- backend/Server/Authentication/views.py
+++ backend/Server/Authentication/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Response
+from rest_framework import status
 
-# Create your views here.
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+from .serializers import LoginPasswordSerializer
+from .permissions import IsNotAuthenticated
+
+
+from Users.models import User
+
+
+
+
+class LoginPasswordAPIView(APIView):
+
+    permission_classes = [IsNotAuthenticated]
+
+    def post(self, request):
+
+        serializer = LoginPasswordSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+
+            phone = serializer.validated_data['phone']
+            user = User.objects.get(phone=phone)
+
+            if not user.is_active:
+                return Response({'error': 'کاربر فعال نیست'}, status=status.HTTP_401_UNAUTHORIZED)
+            
+            refresh = RefreshToken.for_user(user)
+            
+            return Response(
+                {
+                    'refresh': str(refresh), 
+                    'access': str(refresh.access_token),
+                    'user_type': user.user_type
+                },
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
