5f6793d3e342818c305abc19e1be19913aff751a
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-13 16:49:23 +0330

    The UserRegisterOneTimePassword model is added to the AUthentication app.

--- backend/Server/Authentication/models.py
+++ backend/Server/Authentication/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from Otps.models import OneTimePassword
+
+
+
+
+class UserRegisterOneTimePassword(models.Model):
+    otp = models.OneToOneField(
+        OneTimePassword,
+        on_delete=models.CASCADE,
+        related_name="registration_otps"
+    )
+
+    username = models.CharField(max_length=40)
+
+    email = models.EmailField()
+
+    phone = models.CharField(max_length=11)
+
+    password = models.CharField(max_length=128)
+
+    full_name = models.CharField(max_length=255)
+
+    password_conf = models.CharField(max_length=255)
+
+    user_type = models.CharField(max_length=3)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "User Register otp"
+        verbose_name_plural = ""
+
+    def __str__(self):
+        return f"{self.username} - {self.otp.token} - {self.user_type}"
