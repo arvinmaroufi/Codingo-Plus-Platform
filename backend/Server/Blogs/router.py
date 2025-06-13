4e20df86ffb6d750f15fbaa25739e2808d216677
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-13 04:40:08 +0430

    MainCategoryRouter is added to router.py file.

--- backend/Server/Blogs/router.py
+++ backend/Server/Blogs/router.py
@@ -27,3 +27,27 @@ class BlogRouter(routers.DefaultRouter):
             ])),
         ]
         return custom_urls
+    
+    
+class MainCategoryRouter(routers.DefaultRouter):
+
+    def __init__(self):
+        super().__init__()
+        self.register(r'', views.MainCategoryViewSet, basename='blog')
+
+    def get_urls(self):
+        custom_urls = [
+            path('', include([
+                path('', views.MainCategoryViewSet.as_view({'get': 'list'})),
+                path('create/', views.MainCategoryViewSet.as_view({'post': 'create'})),
+                path('<slug:slug>/', include([
+                    # Basic detail route: GET, PUT, DELETE.
+                    path('', views.MainCategoryViewSet.as_view({
+                        'get': 'retrieve',
+                        'put': 'update',
+                        'delete': 'destroy'
+                    })),
+                ])),
+            ])),
+        ]
+        return custom_urls
