991b43005152e537d0e7c45ff578c78a631cecf6
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-13 04:41:25 +0430

    MainCategoryRouter is added to urls.py file.

--- backend/Server/Blogs/urls.py
+++ backend/Server/Blogs/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
-from .router import BlogRouter
+from .router import BlogRouter, MainCategoryRouter
 
 
 app_name = 'Blogs'
 
 
 blog_router = BlogRouter()
+main_category_router = MainCategoryRouter()
 
 urlpatterns = [
     path('blogs/', include(blog_router.get_urls())),
+    path('main-categories/', include(main_category_router.get_urls())),
 ]
