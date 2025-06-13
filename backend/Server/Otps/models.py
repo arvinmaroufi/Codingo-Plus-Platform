049c1cb41b025fab66d18b103c069dacec79731e
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-13 12:45:51 +0330

    The OneTimePassword model ios added to the Otps app.

--- backend/Server/Otps/models.py
+++ backend/Server/Otps/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+import uuid
+
+
+
+class OneTimePassword(models.Model):
+    class OtpStatus(models.TextChoices):
+        EXPIRED = 'EXP' 
+        ACTIVE = 'ACT'
+        USED = 'USE'
+
+    status = models.CharField(
+        max_length=3,
+        choices=OtpStatus.choices,
+        default=OtpStatus.ACTIVE
+    )
+    
+    token = models.UUIDField(verbose_name="توکن", primary_key=True, default=uuid.uuid4)
+    
+    code = models.CharField(max_length=6)
+    
+    expiration = models.DateTimeField(blank=True, null=True)
+
+    is_used = models.BooleanField(default=False, verbose_name="استفاده شده؟")
+    
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="تاریخ ایجاد"
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="تاریخ بروزرسانی"
+    )
+
+    class Meta:
+        verbose_name = "رمز یکبار مصرف"
+        verbose_name_plural = "رمز های یکبار مصرف"
+
+    def __str__(self):
+        return f'{self.status}----{self.code}----{self.token}'
+
+    def get_expiration(self):
+        expiration = self.created_at + timezone.timedelta(minutes=2)
+        self.expiration = expiration
+        self.save()
+
+    def status_validation(self):
+        if self.is_used == True:
+            self.status = 'USE'
+        if self.expiration <= timezone.now():
+            if not self.status == 'USE':
+                self.status = 'EXP'
+                return self.status
+            else:
+                return self.status
+        else:
+            return self.status
