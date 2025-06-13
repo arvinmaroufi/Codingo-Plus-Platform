513c474d4981948c914b2e7fd875e71f3c0c7bea
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-13 04:35:39 +0430

    The MainCategorySerializer is added with create and update methods.

--- backend/Server/Blogs/serializers.py
+++ backend/Server/Blogs/serializers.py
@@ -1,6 +1,33 @@
 from rest_framework import serializers
-from .models import Blog, SubCategory, BlogContent
+from .models import Blog, SubCategory, BlogContent, MainCategory
 
+
+
+class MainCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MainCategory
+        fields = [
+            'title',
+            'slug',
+            'icon',
+            'description',
+            'created_at',
+            'updated_at',
+        ]
+        
+    
+    def create(self, validated_data):
+        return MainCategory.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.slug = validated_data.get('slug', instance.slug)
+        instance.icon = validated_data.get('icon', instance.icon)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        
+        return instance
+    
 
 
 class BlogContentSerializer(serializers.ModelSerializer):
