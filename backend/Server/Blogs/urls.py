b5e2026d2813b276e249f579de9665f025c72b99
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-18 09:06:26 +0430

    CommentRouter is added to urls.py file.

--- backend/Server/Blogs/urls.py
+++ backend/Server/Blogs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .router import BlogRouter, MainCategoryRouter, SubCategoryRouter, TagRouter, CommentReplyRouter
+from .router import BlogRouter, MainCategoryRouter, SubCategoryRouter, TagRouter, CommentReplyRouter, CommentRouter
 
 
 app_name = 'Blogs'
@@ -9,11 +9,13 @@ blog_router = BlogRouter()
 main_category_router = MainCategoryRouter()
 sub_category_router = SubCategoryRouter()
 tag_router = TagRouter()
+comment_router = CommentRouter()
 comment_reply_router = CommentReplyRouter()
 
 urlpatterns = [
     path('blogs/', include(blog_router.get_urls())),
     path('sub-categories/', include(sub_category_router.get_urls())),
     path('tags/', include(tag_router.get_urls())),
+    path('comments/', include(comment_router.get_urls())),
     path('replies/', include(comment_reply_router.get_urls())),
 ]
