a67af89950794516d1293cdc899951347c778b8f
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-24 12:55:07 +0330

    The HeroSection component becomes a full daynamic componenet that contains alot off data in it.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,24 @@
+import { BsCollectionPlay } from "react-icons/bs";
+
+
 import HeroSection from "@/components/HeroSection";
+
+
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <HeroSection />
+        <HeroSection
+          title="مجموعه ی کدینگو"
+          mainText="آموزش توسعه و مهندسی نرم افزار"
+          subText="به مجموعه ی کدینگو خوش آمدید"
+
+          buttonTitle="مشاهده ی دوره ها"
+          buttonIcon={ <BsCollectionPlay /> }
+          buttonPosition="right"
+          buttonUrl="/courses"
+        />
       </div>
     </main>
   );
--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -4,7 +4,30 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 
 
-const HeroSection = () => {
+
+
+const HeroSection = ({
+  title,
+  mainText,
+  subText,
+
+  buttonTitle,
+  buttonIcon,
+  buttonPosition,
+  buttonUrl,
+  buttonClasses,
+} :
+{
+  title: string;
+  mainText: string;
+  subText: string;
+
+  buttonTitle: string;
+  buttonIcon?: React.ReactNode;
+  buttonPosition: string;
+  buttonUrl: string;
+  buttonClasses?: string;
+}) => {
   return (
     <div className="relative pb-5 pt-12">
 
@@ -25,9 +48,7 @@ const HeroSection = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <p className="uppercase tracking-widest text-xs text-center text-main-text-light dark:text-main-text-dark max-w-80">
-            کدینگو پلاس
-          </p>
+          <p className="uppercase tracking-widest text-xs text-center text-main-text-light dark:text-main-text-dark max-w-80">{ title }</p>
 
           {/**
            *  Link: https://ui.aceternity.com/components/text-generate-effect
@@ -35,19 +56,18 @@ const HeroSection = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="آموزش توسعه و مهندسی نرم افزار"
+            words={mainText}
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-main-text-light dark:text-main-text-dark">
-            به مجموعه ی کدینگو خوش آمدید
-          </p>
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-main-text-light dark:text-main-text-dark">{ subText }</p>
 
-          <a href="#about">
+          <a href={buttonUrl}>
             <MagicButton
-              title="مشاهده ی دوره ها"
-              icon={<FaLocationArrow />}
-              position="right"
+              title={buttonTitle}
+              icon={buttonIcon}
+              position={buttonPosition}
+              otherClasses={buttonClasses}
             />
           </a>
         </div>
