94666104d11de4195ce0709d3ede0501b4969059
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-24 15:57:59 +0330

    The register page ui and textes are optimized and the MagicButton updated.

--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -4,7 +4,8 @@ import { Label } from "@/components/ui/Label";
 import { Input } from "@/components/ui/Input";
 import { cn } from "@/lib/utils";
 
-import { IconBrandGithub,  IconBrandGoogle } from "@tabler/icons-react";
+import { MdLockReset } from "react-icons/md";
+import { IoLogIn } from "react-icons/io5";
 import HeroSection from "@/components/HeroSection";
 
 
@@ -19,84 +20,73 @@ export default function RegisterPage() {
 
 
     return (
-        <main className="flex justify-between items-center gap-6 flex-col mx-auto sm:px-10 px-5">
+        <main className="flex justify-evenly items-center gap-6 flex-col mx-auto sm:px-10 px-5">
           <HeroSection
-            title="مجموعه ی کدینگو"
-            mainText="آموزش توسعه و مهندسی نرم افزار"
-            subText="به مجم.عه ی کدینگو خوش آمدید"
+            title="ثبت نام"
+            mainText="با ثبت نام در مجموعه کدینگو می توانید به دوره های ما دسترسی داشته باشید"
+            subText="اگر حساب کاربری دارید می توانید وارد شوید"
 
-            buttonTitle="مشاهده ی دوره ها"
+            buttonTitle="ورود"
+            buttonIcon={<IoLogIn />}
             buttonPosition="right"
             buttonUrl="/courses"
+            buttonClasses="font-bold"
           />
-          <div className="shadow-input mx-auto max-w-md rounded-none bg-highlight-text-dark  p-4 md:rounded-2xl md:p-8 dark:bg-main-text-light">
-              <h2 className="text-xl font-bold text-main-text-light dark:text-main-text-dark">
-                  به کدنیگو پلاس خوش آمدید
-              </h2>
-              <p className="mt-2 max-w-sm text-sm text-main-text-light dark:text-main-text-dark">
-                برای ورود به دنیای عمیق و زیبای برنامه نویسی و مهدنسی نرم لافز ار به ما ملحق شوید
-              </p>
-          
-              <form className="my-8" onSubmit={handleSubmit}>
-                {/* phone */}
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="phone">شماره تلفن</Label>
-                  <Input id="phone" placeholder="0912123456789" type="phone" />
-                </LabelInputContainer>
+          <div className="flex items-center justify-between shadow-input mx-auto max-w-lg bg-slate-50 p-4 rounded-2xl md:p-8 dark:bg-slate-800 w-[80vw]">
+            <form className="w-full" onSubmit={handleSubmit}>
+              {/* phone */}
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="phone">شماره تلفن</Label>
+                <Input id="phone" placeholder="0912123456789" type="phone" />
+              </LabelInputContainer>
 
-                {/* email */}
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="email">ایمیل</Label>
-                  <Input id="email" placeholder="codingo-plus@gmail.com" type="email" />
-                </LabelInputContainer>
+              {/* email */}
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="email">ایمیل</Label>
+                <Input id="email" placeholder="codingo-plus@gmail.com" type="email" />
+              </LabelInputContainer>
 
-                {/* username */}
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="username">نام کاربری</Label>
-                  <Input id="username" placeholder="Codingo-Plus" type="username" />
-                </LabelInputContainer>
+              {/* username */}
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="username">نام کاربری</Label>
+                <Input id="username" placeholder="Codingo-Plus" type="username" />
+              </LabelInputContainer>
 
-                {/* password */}
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="password">رمز عبور</Label>
-                  <Input id="password" placeholder="••••••••" type="password" />
-                </LabelInputContainer>
+              {/* password */}
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="password">رمز عبور</Label>
+                <Input id="password" placeholder="••••••••" type="password" />
+              </LabelInputContainer>
 
-                {/* password_conf */}
-                <LabelInputContainer className="mb-4">
-                  <Label htmlFor="password_conf">تکرار رمز عبور</Label>
-                  <Input id="password_conf" placeholder="••••••••" type="password" />
-                </LabelInputContainer>
+              {/* password_conf */}
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="password_conf">تکرار رمز عبور</Label>
+                <Input id="password_conf" placeholder="••••••••" type="password" />
+              </LabelInputContainer>
+      
+              <button
+                className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-base-dark to-primary-light font-bold text-highlight-text-light shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-base-light dark:from-base-light dark:to-primary-dark dark:text-main-text-light dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
+                type="submit"
+              >
+                ثبت نام
+              <BottomGradient />
+              </button>
+            
+              <div className="my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-primary-dark" />
         
-                <button
-                  className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-base-dark to-primary-light font-medium text-highlight-text-light shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-base-light dark:from-base-light dark:to-primary-dark dark:text-main-text-light dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
-                  type="submit"
-                >
-                  ورود
-                <BottomGradient />
+              <div className="flex flex-row gap-6">
+                <button className="group/btn shadow-input relative flex h-10 w-full items-center justify-evenly space-x-2 rounded-md bg-primary-light px-4 font-bold dark:bg-primary-dark dark:shadow-[0px_0px_1px_1px_#262626]">
+                    <IoLogIn className="h-4 w-4 text-main-text-light dark:text-highlight-text-dark" />
+                    <span className="text-sm text-main-text-light dark:text-highlight-text-dark">ورود با حساب کاربری</span>
+                    <BottomGradient />
                 </button>
-              
-                <div className="my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-primary-dark" />
-          
-                <div className="flex flex-col space-y-4">
-                  <button
-                      className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
-                      type="submit"
-                  >
-                      <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-                      <span className="text-sm text-neutral-700 dark:text-neutral-300">GitHub</span>
-                      <BottomGradient />
-                  </button>
-                  <button
-                      className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
-                      type="submit"
-                  >
-                      <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-                      <span className="text-sm text-neutral-700 dark:text-neutral-300">Google</span>
-                      <BottomGradient />
-                  </button>
-                </div>
-              </form>
+                <button className="group/btn shadow-input relative flex h-10 w-full items-center justify-evenly space-x-2 rounded-md bg-primary-light px-4 font-bold dark:bg-primary-dark dark:shadow-[0px_0px_1px_1px_#262626]">
+                    <MdLockReset className="h-4 w-4 text-main-text-light dark:text-highlight-text-dark" />
+                    <span className="text-sm text-main-text-light dark:text-highlight-text-dark">بازیابی رمز عبور</span>
+                    <BottomGradient />
+                </button>
+              </div>
+            </form>
           </div>
         </main>
     )
--- frontend/src/components/MagicButton.tsx
+++ frontend/src/components/MagicButton.tsx
@@ -15,14 +15,14 @@ const MagicButton = ({
 }) => {
   return (
     <button
-      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
+      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-xl p-[1px] focus:outline-none"
       onClick={handleClick}
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#DAEBE7_0%,#04364C_50%,#63C9DD_100%)]" />
 
       {/* remove px-3 py-1, add px-5 gap-2 */}
       <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-primary-light dark:bg-primary-dark px-7 text-sm font-medium text-main-text-dark dark:text-main-text-dark backdrop-blur-3xl gap-2 ${otherClasses}`}
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-xl bg-primary-light dark:bg-primary-dark px-7 text-sm font-bold text-main-text-dark dark:text-main-text-dark backdrop-blur-3xl gap-3 ${otherClasses}`}
       >
         {position === "left" && icon}
         {title}
