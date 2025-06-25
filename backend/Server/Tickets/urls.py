eaf3f15b69a9a8df03498c3be0065f9a26a32484
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-25 11:57:48 +0430

    TicketMessageRouter is added to urls.py file.

--- backend/Server/Tickets/urls.py
+++ backend/Server/Tickets/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .router import DepartmentRouter, TicketRouter
+from .router import DepartmentRouter, TicketRouter, TicketMessageRouter
 
 
 app_name = 'Tickets'
@@ -7,9 +7,11 @@ app_name = 'Tickets'
 
 department_router = DepartmentRouter()
 ticket_router = TicketRouter()
+ticket_message_router = TicketMessageRouter()
 
 
 urlpatterns = [
     path('departments/', include(department_router.get_urls())),
     path('tickets/', include(ticket_router.get_urls())),
+    path('ticket-messages/', include(ticket_message_router.get_urls())),
 ]
