362c66459986835bf995c77d909d76fce7cd2387
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-25 11:40:14 +0430

    TicketRouter is added to urls.py file.

--- backend/Server/Tickets/urls.py
+++ backend/Server/Tickets/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
-from .router import DepartmentRouter
+from .router import DepartmentRouter, TicketRouter
 
 
 app_name = 'Tickets'
 
 
 department_router = DepartmentRouter()
+ticket_router = TicketRouter()
 
 
 urlpatterns = [
     path('departments/', include(department_router.get_urls())),
+    path('tickets/', include(ticket_router.get_urls())),
 ]
