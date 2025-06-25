d363f0dd225677ffce3f3012c3efbeb98be717d0
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-25 14:05:54 +0330

    The ub category router and urls added.

--- backend/Server/Courses/urls.py
+++ backend/Server/Courses/urls.py
@@ -6,9 +6,11 @@ from . import routers
 
 
 main_category_router = routers.MainCategoryRouter()
+sub_category_router = routers.SubCategoryRouter()
 
 
 app_name = 'Courses'
 urlpatterns = [
     path('main-categories/', include(main_category_router.get_urls())),
+    path('sub-categories/', include(sub_category_router.get_urls())),
 ]
