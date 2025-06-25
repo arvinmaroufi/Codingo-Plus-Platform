b4558dbd8642f1552ad098f0ca8dfb93d3770097
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-25 12:05:10 +0430

    TicketAttachmentRouter is added to urls.py file.

--- backend/Server/Tickets/urls.py
+++ backend/Server/Tickets/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .router import DepartmentRouter, TicketRouter, TicketMessageRouter
+from .router import DepartmentRouter, TicketRouter, TicketMessageRouter, TicketAttachmentRouter
 
 
 app_name = 'Tickets'
@@ -8,10 +8,12 @@ app_name = 'Tickets'
 department_router = DepartmentRouter()
 ticket_router = TicketRouter()
 ticket_message_router = TicketMessageRouter()
+ticket_attachment_router = TicketAttachmentRouter()
 
 
 urlpatterns = [
     path('departments/', include(department_router.get_urls())),
     path('tickets/', include(ticket_router.get_urls())),
     path('ticket-messages/', include(ticket_message_router.get_urls())),
+    path('ticket-attachments/', include(ticket_attachment_router.get_urls())),
 ]
