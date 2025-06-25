2a3db4838e5243470e907d3d9677f5fe11887332
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-25 16:19:31 +0330

    The TagRouter and urls added.

--- backend/Server/Courses/routers.py
+++ backend/Server/Courses/routers.py
@@ -46,3 +46,23 @@ class SubCategoryRouter(routers.DefaultRouter):
         ]
         return custom_urls
     
+
+class TagRouter(routers.DefaultRouter):
+
+    def __init__(self):
+        super().__init__()
+        self.register(r'', views.TagViewSet, basename='tags')
+
+    def get_urls(self):
+        custom_urls = [
+            path('', include([
+                path('', views.TagViewSet.as_view({'get': 'list', 'post': 'create'})),
+
+                path('<slug:slug>/', include([
+                    # Basic detail route: GET, PUT, DELETE.
+                    path('', views.TagViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'})),
+                ])),
+            ])),
+        ]
+        return custom_urls
+    
--- backend/Server/Courses/urls.py
+++ backend/Server/Courses/urls.py
@@ -3,14 +3,16 @@ from . import routers
 
 
 
+app_name = "Courses"
 
 
 main_category_router = routers.MainCategoryRouter()
 sub_category_router = routers.SubCategoryRouter()
+tags_router = routers.SubCategoryRouter()
 
 
-app_name = 'Courses'
 urlpatterns = [
     path('main-categories/', include(main_category_router.get_urls())),
     path('sub-categories/', include(sub_category_router.get_urls())),
+    path('tags/', include(tags_router.get_urls())),
 ]
