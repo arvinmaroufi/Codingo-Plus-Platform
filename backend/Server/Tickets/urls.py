a8e7ba7ef319b2db0fb879eff8b80767fa0e49bd
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-06-26 11:15:06 +0430

    CourseTicketMessageRouter is added to urls.py file.

--- backend/Server/Tickets/urls.py
+++ backend/Server/Tickets/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .router import DepartmentRouter, TicketRouter, TicketMessageRouter, TicketAttachmentRouter, CourseDepartmentRouter, CourseTicketRouter
+from .router import DepartmentRouter, TicketRouter, TicketMessageRouter, TicketAttachmentRouter, CourseDepartmentRouter, CourseTicketRouter, CourseTicketMessageRouter
 
 
 app_name = 'Tickets'
@@ -11,6 +11,7 @@ ticket_message_router = TicketMessageRouter()
 ticket_attachment_router = TicketAttachmentRouter()
 course_department_router = CourseDepartmentRouter()
 course_ticket_router = CourseTicketRouter()
+course_ticket_message_router = CourseTicketMessageRouter()
 
 
 urlpatterns = [
@@ -18,6 +19,7 @@ urlpatterns = [
     path('tickets/', include(ticket_router.get_urls())),
     path('ticket-messages/', include(ticket_message_router.get_urls())),
     path('ticket-attachments/', include(ticket_attachment_router.get_urls())),
-    path('course_departments/', include(course_department_router.get_urls())),
-    path('course_tickets/', include(course_ticket_router.get_urls())),
+    path('course-departments/', include(course_department_router.get_urls())),
+    path('course-tickets/', include(course_ticket_router.get_urls())),
+    path('course-ticket-messages/', include(course_ticket_message_router.get_urls())),
 ]
