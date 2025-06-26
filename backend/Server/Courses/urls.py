a9037c3e6ab0c4654dea4343f8d20303b7266eac
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-26 14:55:27 +0330

    The courses route and urls added.

--- backend/Server/Courses/urls.py
+++ backend/Server/Courses/urls.py
@@ -9,10 +9,12 @@ app_name = "Courses"
 main_category_router = routers.MainCategoryRouter()
 sub_category_router = routers.SubCategoryRouter()
 tags_router = routers.SubCategoryRouter()
+course_router = routers.CourseRouter()
 
 
 urlpatterns = [
     path('main-categories/', include(main_category_router.get_urls())),
     path('sub-categories/', include(sub_category_router.get_urls())),
     path('tags/', include(tags_router.get_urls())),
+    path('courses/', include(course_router.get_urls())),
 ]
