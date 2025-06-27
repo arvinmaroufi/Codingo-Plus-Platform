ea3df738eb29dc2dddf4b50e0e44647d1700c747
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-27 10:51:58 +0330

    Course sessions routes are added to the main url.

--- backend/Server/Courses/urls.py
+++ backend/Server/Courses/urls.py
@@ -12,7 +12,8 @@ tags_router = routers.SubCategoryRouter()
 course_router = routers.CourseRouter()
 course_faqs_router = routers.CourseFaqRouter()
 course_contents_router = routers.CourseContentRouter()
-course_chapters = routers.CourseChapterRouter()
+course_chapters_router = routers.CourseChapterRouter()
+course_sessions_router = routers.CourseSessionRouter()
 
 
 urlpatterns = [
@@ -22,5 +23,6 @@ urlpatterns = [
     path('courses/', include(course_router.get_urls())),
     path('course-faqs/', include(course_faqs_router.get_urls())),
     path('course-contents/', include(course_contents_router.get_urls())),
-    path('course-chapters/', include(course_chapters.get_urls())),
+    path('course-sessions/', include(course_sessions_router.get_urls())),
+    path('course-chapters/', include(course_chapters_router.get_urls())),
 ]
