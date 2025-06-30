c1b3a1d3435e1dbd899bf8db61a611613f7e9a09
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-06-30 18:42:33 +0330

    THe ClientProvider provider is added to make the layout server-side SSR with the same sessions provides.

---
+++ frontend/src/providers/ClientProviders.tsx
@@ -0,0 +1,24 @@
+// app/providers/ClientProviders.tsx
+"use client";
+
+import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+import Theme from "@/providers/ThemeProvider";
+import Navbar from "@/components/Navbar";
+
+export default function ClientProviders({
+  children,
+  session,
+}: {
+  children: ReactNode;
+  session?: any;
+}) {
+  return (
+    <SessionProvider session={session} basePath="/api/auth">
+      <Theme>
+        <Navbar />
+        {children}
+      </Theme>
+    </SessionProvider>
+  );
+}
