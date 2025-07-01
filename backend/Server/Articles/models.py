2b264f02635a5c90f06fc3db9de6f9337e50ac6d
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-01 17:40:05 +0430

    MainCategory is added to the article models and migrated to the db.

--- backend/Server/Articles/models.py
+++ backend/Server/Articles/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class MainCategory(models.Model):
+    """Represents main categories for blog posts."""
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name='عنوان دسته بندی'
+    )
+    slug = models.SlugField(
+        max_length=100,
+        unique=True,
+        verbose_name='نامک'
+    )
+    icon = models.FileField(
+        upload_to='Blogs/MainCategory_icons/',
+        verbose_name='آیکون'
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='توضیحات'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='تاریخ ایجاد'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='تاریخ به‌روزرسانی'
+    )
+
+    class Meta:
+        verbose_name = 'دسته بندی والد'
+        verbose_name_plural = 'دسته بندی های والد'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+    
