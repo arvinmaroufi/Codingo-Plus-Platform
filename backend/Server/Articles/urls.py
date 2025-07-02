94d59aa37c933fb608a65486a6a75b84bf93263f
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-02 09:53:14 +0430

    SubCategoryRouter is added to urls.py file.

--- backend/Server/Articles/urls.py
+++ backend/Server/Articles/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
-from .router import MainCategoryRouter
+from .router import MainCategoryRouter, SubCategoryRouter
 
 
 app_name = 'Articles'
 
 
 main_category_router = MainCategoryRouter()
+sub_category_router = SubCategoryRouter()
 
 
 urlpatterns = [
     path('main-categories/', include(main_category_router.get_urls())),
+    path('sub-categories/', include(sub_category_router.get_urls())),
 ]
