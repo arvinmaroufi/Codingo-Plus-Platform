57dbd8beb05d8aa98cd3baf8131f60a300c6ec51
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-02 13:44:33 +0430

    AuthorRouter is added to urls.py file.

--- backend/Server/Articles/urls.py
+++ backend/Server/Articles/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .router import MainCategoryRouter, SubCategoryRouter, TagRouter
+from .router import MainCategoryRouter, SubCategoryRouter, TagRouter, AuthorRouter
 
 
 app_name = 'Articles'
@@ -8,10 +8,12 @@ app_name = 'Articles'
 main_category_router = MainCategoryRouter()
 sub_category_router = SubCategoryRouter()
 tag_router = TagRouter()
+author_router = AuthorRouter()
 
 
 urlpatterns = [
     path('main-categories/', include(main_category_router.get_urls())),
     path('sub-categories/', include(sub_category_router.get_urls())),
     path('tags/', include(tag_router.get_urls())),
+    path('authors/', include(author_router.get_urls())),
 ]
