6c94f9058f6037dafb4dbde72f82dc335d73dd58
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-02 10:00:09 +0430

    TagRouter is added to router.py file.

--- backend/Server/Articles/router.py
+++ backend/Server/Articles/router.py
@@ -50,3 +50,27 @@ class SubCategoryRouter(routers.DefaultRouter):
             ])),
         ]
         return custom_urls
+    
+
+class TagRouter(routers.DefaultRouter):
+
+    def __init__(self):
+        super().__init__()
+        self.register(r'', views.TagViewSet, basename='article')
+
+    def get_urls(self):
+        custom_urls = [
+            path('', include([
+                path('', views.TagViewSet.as_view({'get': 'list'})),
+                path('create/', views.TagViewSet.as_view({'post': 'create'})),
+                path('<slug:slug>/', include([
+                    # Basic detail route: GET, PUT, DELETE.
+                    path('', views.TagViewSet.as_view({
+                        'get': 'retrieve',
+                        'put': 'update',
+                        'delete': 'destroy'
+                    })),
+                ])),
+            ])),
+        ]
+        return custom_urls
