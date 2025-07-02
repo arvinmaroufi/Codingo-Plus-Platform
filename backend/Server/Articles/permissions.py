90795f53414c7108058188aea7979bcc43e632b9
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-02 13:50:24 +0430

    IsAdminOrAuthor is added in permissions.py file.

--- backend/Server/Articles/permissions.py
+++ backend/Server/Articles/permissions.py
@@ -16,3 +16,24 @@ class IsAdminOrReadOnly(BasePermission):
 
         if request.user.is_staff:
             return True
+
+
+class IsAdminOrAuthor(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+
+        if request.user.is_staff:
+            return True
+        
+        if request.user.user_type == "TE":
+            return True
+        
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return request.user.is_staff or obj.author == request.user
+    
