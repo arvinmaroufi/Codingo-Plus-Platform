845cff961f374e2037f667f9eccce6342986d974
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-02 22:09:59 +0330

    The user registration opration optimized and the messages and erorrs in api structured and optimized and also translated to the persian.

--- backend/Server/Authentication/urls.py
+++ backend/Server/Authentication/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('login-password/', views.LoginPasswordAPIView.as_view(), name="login-password"),
 
     path('register-request-otp/', views.UserRegisterRequestOtpAPIView.as_view(), name="register-request-otp"),
-    path('register-validate-otp/', views.UserRegisterValidateOtpAPIView.as_view(), name="register-validate-otp"),
+    path('register-validate-otp/<uuid:token>/', views.UserRegisterValidateOtpAPIView.as_view(), name="register-validate-otp"),
 
     path('login-request-otp/', views.UserLoginRequestOtpAPIView.as_view(), name="login-request-otp"),
     path('login-validate-otp/', views.UserLoginValidateOtpAPIView.as_view(), name="login-validate-otp"),
