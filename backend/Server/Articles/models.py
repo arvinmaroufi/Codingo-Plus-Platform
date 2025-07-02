f590a603a79170e633e8439260c1359dd78feb33
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-02 08:24:31 +0430

    Article is added to the article models and migrated to the db.

--- backend/Server/Articles/models.py
+++ backend/Server/Articles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from Users.models import User
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.utils import timezone
 
 
 class MainCategory(models.Model):
@@ -150,3 +152,88 @@ class Author(models.Model):
     
     def __str__(self):
         return self.full_name
+
+
+class Article(models.Model):
+    class PublishStatusChoices(models.TextChoices):
+        DRAFT = 'DR', 'پیش نویس شود'
+        PUBLISHED = 'PD', 'منتشر شود'
+
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        verbose_name='نویسنده مقاله'
+    )
+    sub_category = models.ForeignKey(
+        SubCategory,
+        on_delete=models.CASCADE,
+        related_name='blogs',
+        verbose_name='دسته بندی'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+        related_name='articles',
+        verbose_name='برچسب'
+    )
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name='عنوان مقاله'
+    )
+    slug = models.SlugField(
+        max_length=100,
+        unique=True,
+        verbose_name='نامک'
+    )
+    description = RichTextUploadingField(
+        verbose_name='توضیحات'
+    )
+    poster = models.ImageField(
+        upload_to="Articles/posters/",
+        blank=True,
+        null=True,
+        verbose_name='پوستر مقاله'
+    )
+    banner = models.ImageField(
+        upload_to="Articles/banners/",
+        blank=True,
+        null=True,
+        verbose_name='بنر مقاله'
+    )
+    status = models.CharField(
+        choices=PublishStatusChoices.choices,
+        max_length=10,
+        default=PublishStatusChoices.DRAFT,
+        verbose_name='وضعیت'
+    )
+    views = models.PositiveIntegerField(
+        default=0,
+        verbose_name='بازدید ها'
+    )
+    published_at = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name='تاریخ انتشار'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='تاریخ ساخت'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='تاریخ بروزرسانی'
+    )
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+    def publish(self):
+        self.status = Article.PublishStatusChoices.PUBLISHED
+        self.published_at = timezone.now()
+        self.save()
