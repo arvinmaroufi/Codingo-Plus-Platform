1447b0e2b2bea68147401a54e4b538b4ea1ac4c9
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-02 09:52:21 +0430

    SubCategoryViewSet is added to views.py file.

--- backend/Server/Articles/views.py
+++ backend/Server/Articles/views.py
@@ -4,8 +4,8 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.views import Response, APIView
 from rest_framework import status
 
-from .models import MainCategory
-from .serializers import MainCategorySerializer
+from .models import MainCategory, SubCategory
+from .serializers import MainCategorySerializer, SubCategorySerializer
 from .permissions import IsAdminOrReadOnly
 
 
@@ -43,3 +43,38 @@ class MainCategoryViewSet(ViewSet):
         self.check_object_permissions(request=request, obj=instance)
         instance.delete()
         return Response({'message': 'The MainCategory is deleted.'}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+class SubCategoryViewSet(ViewSet):
+
+    permission_classes = [IsAdminOrReadOnly]
+    lookup_field = 'slug'
+    
+    def list(self):
+        queryset = SubCategory.objects.all()
+        serializer = SubCategorySerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        serializer = SubCategorySerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'message': 'The SubCategory is added.'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    def update(self, request, slug):
+        instance = get_object_or_404(SubCategory, slug=slug)
+        self.check_object_permissions(request=request, obj=instance)
+        serializer = SubCategorySerializer(instance, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'message': 'The SubCategory is updated.'}, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def destroy(self, request, slug):
+        instance = get_object_or_404(SubCategory, slug=slug)
+        self.check_object_permissions(request=request, obj=instance)
+        instance.delete()
+        return Response({'message': 'The SubCategory is deleted.'}, status=status.HTTP_204_NO_CONTENT)
