14cd2f16f117071f0c7b0733fc97a4a0f5529340
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-02 13:57:12 +0430

    The ArticleSerializer is added with create and update methods.

--- backend/Server/Articles/serializers.py
+++ backend/Server/Articles/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import MainCategory, SubCategory, Tag, Author, ArticleContent
+from .models import MainCategory, SubCategory, Tag, Author, ArticleContent, Article
 
 
 
@@ -158,6 +158,95 @@ class ArticleContentSerializer(serializers.ModelSerializer):
         instance.video = validated_data.get('video', instance.video)
         instance.link = validated_data.get('link', instance.link)
         instance.order = validated_data.get('order', instance.order)
+        instance.save()
+        
+        return instance
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    author_id = serializers.IntegerField(write_only=True)
+    sub_category_slug = serializers.CharField(write_only=True)
+    tags_slugs = serializers.ListField(child=serializers.CharField(), write_only=True, required=False)
+    contents = ArticleContentSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Article
+        fields = [
+            'id',
+            'author',
+            'author_id',
+            'sub_category_slug',
+            'tags_slugs',
+            'title',
+            'slug',
+            'description',
+            'poster',
+            'banner',
+            'status',
+            'views',
+            'published_at',
+            'created_at',
+            'updated_at',
+            'contents',
+        ]
+        
+    def create(self, validated_data):
+        tags_slugs = validated_data.pop('tags_slugs', [])
+        sub_category_slug = validated_data.pop('sub_category_slug')
+        author_id = validated_data.pop('author_id')
+        
+        try:
+            sub_category = SubCategory.objects.get(slug=sub_category_slug)
+        except SubCategory.DoesNotExist:
+            raise serializers.ValidationError({"sub_category_slug": "SubCategory does not exist."})
+            
+        try:
+            author = Author.objects.get(id=author_id)
+        except Author.DoesNotExist:
+            raise serializers.ValidationError({"author_id": "Author does not exist."})
+            
+        article = Article.objects.create(
+            author=author,
+            sub_category=sub_category,
+            **validated_data
+        )
+        
+        if tags_slugs:
+            tags = Tag.objects.filter(slug__in=tags_slugs)
+            article.tags.set(tags)
+            
+        return article
+    
+    def update(self, instance, validated_data):
+        tags_slugs = validated_data.pop('tags_slugs', None)
+        sub_category_slug = validated_data.pop('sub_category_slug', None)
+        author_id = validated_data.pop('author_id', None)
+        
+        if sub_category_slug:
+            try:
+                sub_category = SubCategory.objects.get(slug=sub_category_slug)
+                instance.sub_category = sub_category
+            except SubCategory.DoesNotExist:
+                raise serializers.ValidationError({"sub_category_slug": "SubCategory does not exist."})
+                
+        if author_id:
+            try:
+                author = Author.objects.get(id=author_id)
+                instance.author = author
+            except Author.DoesNotExist:
+                raise serializers.ValidationError({"author_id": "Author does not exist."})
+                
+        if tags_slugs is not None:
+            tags = Tag.objects.filter(slug__in=tags_slugs)
+            instance.tags.set(tags)
+            
+        instance.title = validated_data.get('title', instance.title)
+        instance.slug = validated_data.get('slug', instance.slug)
+        instance.description = validated_data.get('description', instance.description)
+        instance.poster = validated_data.get('poster', instance.poster)
+        instance.banner = validated_data.get('banner', instance.banner)
+        instance.status = validated_data.get('status', instance.status)
         instance.save()
         
         return instance
