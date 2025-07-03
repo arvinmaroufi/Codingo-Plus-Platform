f2abd885ac88e85fe40ccbbe2e060a18388ecb19
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-03 10:01:36 +0330

    The token refresh and verify views added from simple_jwt package to the urls of authentication app.

--- backend/Server/Authentication/urls.py
+++ backend/Server/Authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 from . import views
 
 
 
 app_name = "authentication"
+
 
 urlpatterns = [
     path('login-password/', views.LoginPasswordAPIView.as_view(), name="login-password"),
@@ -14,5 +15,7 @@ urlpatterns = [
 
     path('login-request-otp/', views.UserLoginRequestOtpAPIView.as_view(), name="login-request-otp"),
     path('login-validate-otp/', views.UserLoginValidateOtpAPIView.as_view(), name="login-validate-otp"),
-]
 
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
