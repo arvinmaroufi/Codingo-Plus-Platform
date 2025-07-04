6966829f03a395f91bf30fac981b078f3f777f7c
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-04 11:57:39 +0430

    CommentReplyRouter is added to urls.py file.

--- backend/Server/Books/urls.py
+++ backend/Server/Books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .router import MainCategoryRouter, SubCategoryRouter, TagRouter, BookRouter
+from .router import MainCategoryRouter, SubCategoryRouter, TagRouter, BookRouter, CommentReplyRouter
 
 
 app_name = 'Books'
@@ -9,6 +9,7 @@ main_category_router = MainCategoryRouter()
 sub_category_router = SubCategoryRouter()
 tag_router = TagRouter()
 book_router = BookRouter()
+comment_reply_router = CommentReplyRouter()
 
 
 urlpatterns = [
@@ -16,4 +17,5 @@ urlpatterns = [
     path('sub-categories/', include(sub_category_router.get_urls())),
     path('tags/', include(tag_router.get_urls())),
     path('books/', include(book_router.get_urls())),
+    path('replies/', include(comment_reply_router.get_urls())),
 ]
