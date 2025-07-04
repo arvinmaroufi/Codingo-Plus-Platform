e953a02b00d59414ce22be6f841d6e8894d51982
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-04 10:56:51 +0430

    MainCategoryRouter is added to router.py file.

---
+++ backend/Server/Books/router.py
@@ -0,0 +1,28 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+
+    
+class MainCategoryRouter(routers.DefaultRouter):
+
+    def __init__(self):
+        super().__init__()
+        self.register(r'', views.MainCategoryViewSet, basename='article')
+
+    def get_urls(self):
+        custom_urls = [
+            path('', include([
+                path('', views.MainCategoryViewSet.as_view({'get': 'list'})),
+                path('create/', views.MainCategoryViewSet.as_view({'post': 'create'})),
+                path('<slug:slug>/', include([
+                    # Basic detail route: GET, PUT, DELETE.
+                    path('', views.MainCategoryViewSet.as_view({
+                        'get': 'retrieve',
+                        'put': 'update',
+                        'delete': 'destroy'
+                    })),
+                ])),
+            ])),
+        ]
+        return custom_urls
