e6e6206927937f8618f8c6c7cea20d6f2d4e7bd6
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-04 11:50:29 +0430

    BookRouter is added to urls.py file.

--- backend/Server/Books/urls.py
+++ backend/Server/Books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .router import MainCategoryRouter, SubCategoryRouter, TagRouter
+from .router import MainCategoryRouter, SubCategoryRouter, TagRouter, BookRouter
 
 
 app_name = 'Books'
@@ -8,10 +8,12 @@ app_name = 'Books'
 main_category_router = MainCategoryRouter()
 sub_category_router = SubCategoryRouter()
 tag_router = TagRouter()
+book_router = BookRouter()
 
 
 urlpatterns = [
     path('main-categories/', include(main_category_router.get_urls())),
     path('sub-categories/', include(sub_category_router.get_urls())),
     path('tags/', include(tag_router.get_urls())),
+    path('books/', include(book_router.get_urls())),
 ]
