b060868050f9052a3ad36a76ecb213718777428a
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-04 11:56:37 +0430

    IsAdminOrCommentAuthor is added in permissions.py file.

--- backend/Server/Books/permissions.py
+++ backend/Server/Books/permissions.py
@@ -16,3 +16,11 @@ class IsAdminOrReadOnly(BasePermission):
 
         if request.user.is_staff:
             return True
+
+
+class IsAdminOrCommentAuthor(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        
+        return request.user.is_staff or obj.user == request.user
