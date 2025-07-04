eb409c479a1d5e73b3f7e7de36499e589aa49f1c
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-04 11:04:44 +0430

    TagRouter is added to urls.py file.

--- backend/Server/Books/urls.py
+++ backend/Server/Books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .router import MainCategoryRouter, SubCategoryRouter
+from .router import MainCategoryRouter, SubCategoryRouter, TagRouter
 
 
 app_name = 'Books'
@@ -7,9 +7,11 @@ app_name = 'Books'
 
 main_category_router = MainCategoryRouter()
 sub_category_router = SubCategoryRouter()
+tag_router = TagRouter()
 
 
 urlpatterns = [
     path('main-categories/', include(main_category_router.get_urls())),
     path('sub-categories/', include(sub_category_router.get_urls())),
+    path('tags/', include(tag_router.get_urls())),
 ]
