0fbb4216881db7fce697c0f4e7c5cc4ac3305790
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-04 05:28:48 +0430

    MainCategory is added to the book models and migrated to the db.

--- backend/Server/Books/models.py
+++ backend/Server/Books/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class MainCategory(models.Model):
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name='عنوان دسته بندی'
+    )
+    slug = models.SlugField(
+        max_length=100,
+        unique=True,
+        verbose_name='نامک'
+    )
+    icon = models.FileField(
+        upload_to='Books/MainCategory_icons/',
+        verbose_name='آیکون'
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='توضیحات'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='تاریخ ایجاد'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='تاریخ به‌روزرسانی'
+    )
+
+    class Meta:
+        verbose_name = 'دسته بندی والد'
+        verbose_name_plural = 'دسته بندی های والد'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
