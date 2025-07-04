ebc089361c0d9e7f6dd87207073dfbf48a8ce24f
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-04 10:56:09 +0430

    MainCategoryViewSet is added to views.py file.

--- backend/Server/Books/views.py
+++ backend/Server/Books/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.viewsets import ViewSet
+from rest_framework.views import Response, APIView
+from rest_framework import status
+
+from .models import MainCategory
+from .serializers import MainCategorySerializer
+from .permissions import IsAdminOrReadOnly
+
+
+
+
+class MainCategoryViewSet(ViewSet):
+
+    permission_classes = [IsAdminOrReadOnly]
+    lookup_field = 'slug'
+    
+    def list(self):
+        queryset = MainCategory.objects.all()
+        serializer = MainCategorySerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        serializer = MainCategorySerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'message': 'The MainCategory is added.'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    def update(self, request, slug):
+        instance = get_object_or_404(MainCategory, slug=slug)
+        self.check_object_permissions(request=request, obj=instance)
+        serializer = MainCategorySerializer(instance, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'message': 'The MainCategory is updated.'}, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def destroy(self, request, slug):
+        instance = get_object_or_404(MainCategory, slug=slug)
+        self.check_object_permissions(request=request, obj=instance)
+        instance.delete()
+        return Response({'message': 'The MainCategory is deleted.'}, status=status.HTTP_204_NO_CONTENT)
