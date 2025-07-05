f0a2a5f5840e8cc187c63c59624f31e2abd69c23
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-05 08:11:23 +0430

    SubCategory is added to the podcast models and migrated to the db.

--- backend/Server/Podcasts/models.py
+++ backend/Server/Podcasts/models.py
@@ -37,3 +37,47 @@ class MainCategory(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class SubCategory(models.Model):
+    parent = models.ForeignKey(
+        MainCategory,
+        on_delete=models.CASCADE,
+        related_name='podcasts_subcategories',
+        verbose_name='دسته بندی والد'
+    )
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name='عنوان دسته بندی'
+    )
+    slug = models.SlugField(
+        max_length=100,
+        unique=True,
+        verbose_name='نامک'
+    )
+    icon = models.FileField(
+        upload_to='Podcasts/SubCategory_icons/',
+        verbose_name='آیکون'
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='توضیحات'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='تاریخ ایجاد'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='تاریخ به‌روزرسانی'
+    )
+
+    class Meta:
+        verbose_name = 'زیر دسته بندی'
+        verbose_name_plural = 'زیر دسته بندی ها'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
