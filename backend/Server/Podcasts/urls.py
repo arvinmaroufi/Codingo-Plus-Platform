42aec3de0cbcd72d73b4c0704cae3185e19a07c5
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-05 10:43:05 +0430

    PodcastRouter is added to urls.py file.

--- backend/Server/Podcasts/urls.py
+++ backend/Server/Podcasts/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
-from .router import MainCategoryRouter, SubCategoryRouter, TagRouter
+from .router import MainCategoryRouter, SubCategoryRouter, TagRouter, PodcastRouter
 
 
 app_name = 'Podcasts'
 main_category_router = MainCategoryRouter()
 sub_category_router = SubCategoryRouter()
 tag_router = TagRouter()
+podcast_router = PodcastRouter()
 
 
 urlpatterns = [
     path('main-categories/', include(main_category_router.get_urls())),
     path('sub-categories/', include(sub_category_router.get_urls())),
     path('tags/', include(tag_router.get_urls())),
+    path('podcasts/', include(podcast_router.get_urls())),
 ]
