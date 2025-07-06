8b91d12814d99d5a5c18e07df11c9f0b52dbcb9c
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-07 00:12:02 +0330

    The update method is added for the SubscriptionSerializer class.

--- backend/Server/Subscriptions/serializers.py
+++ backend/Server/Subscriptions/serializers.py
@@ -38,11 +38,34 @@ class SubscriptionSerializer(serializers.ModelSerializer):
 
     is_active = serializers.SerializerMethodField()
 
+    plan_slug = serializers.CharField(write_only=True, required=False)
+
+
     class Meta:
         model = Subscription
         fields = "__all__"
         read_only_fields = ['user', 'plan', 'status']
-    
-    
+
+
+    def update(self, instance, validated_data):
+
+        plan_slug = validated_data.pop('plan_slug', None)
+        if plan_slug is not None:
+            try:
+                plan =  SubscriptionPlan.objects.get(slug=plan_slug)
+                instance.plan = plan
+            except  SubscriptionPlan.DoesNotExist:
+                raise serializers.ValidationError({'plan_slug': 'پلن پیدا نشد.'})
+        
+        instance.status = validated_data.get('status', instance.status)
+        instance.duration = validated_data.get('duration', instance.duration)
+        instance.starts_at = validated_data.get('starts_at', instance.starts_at)
+        instance.expires_at = validated_data.get('expires_at', instance.expires_at)
+
+        instance.save()
+
+        return instance
+        
+
     def get_is_active(self, obj):
         return obj.is_active()
