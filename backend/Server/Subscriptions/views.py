1a9f0317c0eeada5f14d45292ae0d17aa22f4a0b
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-06 23:58:18 +0330

    The update method is added for the SubscriptionPlanViewSet class.

--- backend/Server/Subscriptions/views.py
+++ backend/Server/Subscriptions/views.py
@@ -36,6 +36,16 @@ class SubscriptionPlanViewSet(viewsets.ViewSet):
             return Response({'erorr': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
+    def update(self, request, slug):
+        instance = get_object_or_404(SubscriptionPlan, slug=slug)
+        self.check_object_permissions(request=request, obj=instance)
+        serializer = SubscriptionPlanSerializer(data=request.data, partial=True, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'پلن آپدیت شد.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'erorr': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class SubscriptionViewSet(viewsets.ViewSet):
 
