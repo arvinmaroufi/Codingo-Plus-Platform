ae5abdcf75c805babec1d7dc3333d109f75f079e
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-06 18:13:52 +0330

    The SubscriptionPlanRouter and SubscriptionRouter are added.

---
+++ backend/Server/Subscriptions/routers.py
@@ -0,0 +1,38 @@
+from django.urls import path, include
+
+from rest_framework import routers
+
+from . import views
+
+
+class SubscriptionPlanRouter(routers.DefaultRouter):
+
+    def __init__(self):
+        super().__init__()
+        self.register(r'', views.SubscriptionPlanViewSet, basename='plans')
+
+    def get_urls(self):
+        custom_urls = [
+            path('', include([
+                path('', views.SubscriptionPlanViewSet.as_view({'get': 'list'})),
+                path('<slug:slug>/', views.SubscriptionPlanViewSet.as_view({'get': 'retrieve'})),
+            ])),
+        ]
+        return custom_urls
+    
+
+class SubscriptionRouter(routers.DefaultRouter):
+
+    def __init__(self):
+        super().__init__()
+        self.register(r'', views.SubscriptionViewSet, basename='subscription')
+
+    def get_urls(self):
+        custom_urls = [
+            path('', include([
+                path('', views.SubscriptionViewSet.as_view({'get': 'list'})),
+                path('<slug:slug>/', views.SubscriptionViewSet.as_view({'get': 'retrieve'})),
+                path('my-subscription/', views.SubscriptionViewSet.as_view({'get': 'my_subscription'})),
+            ])),
+        ]
+        return custom_urls
