fc60debde8c1dc1446cb4e54841504f098612da7
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-08 13:39:14 +0330

    The second step of handeling the reset password opration is implemented in serializers using AccountResetPasswordOTPSerializer.

--- backend/Server/Accounts/serializers.py
+++ backend/Server/Accounts/serializers.py
@@ -48,3 +48,31 @@ class AccountResetPasswordOTPSerializer(serializers.Serializer):
         otp.get_expiration()
 
         return {'phone': reset_password_otp.phone, 'token': token, 'code': code}
+
+
+
+class AccountResetPasswordValidateSerializer(serializers.Serializer):
+
+    code = serializers.CharField(max_length=6, min_length=6, required=True)
+    password = serializers.CharField(max_length=16, min_length=8, required=True)
+    password_conf = serializers.CharField(max_length=16, min_length=8, required=True)
+
+    
+    def validate(self, attrs):
+        otp_token = self.context.get('otp_token')
+        
+        otp = OneTimePassword.objects.get(token=otp_token)
+        reset_password_otp = otp.reset_password.get()
+        
+        if len(attrs['password']) < 8 or len(attrs['password']) > 16:
+            raise serializers.ValidationError({'error': 'رمز عبور باید حداقل 8 کاراکتر و حداکثر 16 کاراکتر طول داشته باشد.'})
+
+        if otp.status_validation() == 'ACT':
+            if otp.code == attrs['code']:
+                # Check if the password and password_conf match
+                if attrs['password'] != attrs['password_conf']:
+                    raise serializers.ValidationError({'erorr': 'رمزهای عبور با هم تطابق ندارند'})
+            else:
+                raise serializers.ValidationError({'error': 'کد یکبار مصرف نامعتبر نیست.'})
+        else:
+            raise serializers.ValidationError({'erorr': 'کد یکبار مصرف فعال نیست'})
