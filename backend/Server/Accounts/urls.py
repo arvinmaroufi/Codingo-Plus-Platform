50f0851558d57399cd98a98f4a934beb2799da28
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-08 23:27:44 +0330

    The new account reset password using otp urls are added.

---
+++ backend/Server/Accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+
+
+
+
+app_name = "Accounts"
+
+
+
+urlpatterns = [
+    path('reset-password-request-otp/', views.ResetPasswordRequestOtpAPIView.as_view(), name="reset_password_request_otp"),
+
+    path('reset-password-validate-otp/<str:token>/', views.ResetPasswordValidateOtpAPIView.as_view(), name="reset_password_validate_otp"),
+]
