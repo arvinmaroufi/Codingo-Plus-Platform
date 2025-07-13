61435ee489b64c2ae1be41f1a3fc99e9ab68ec19
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-13 09:08:34 +0330

    The courses page is added.

---
+++ frontend/src/app/(courses)/courses/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default async function CoursesList() {
+    return (
+        <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+            
+        </main>
+    );
+}
