9ecfa23568320a3d665623ae811781fafe2aa6ee
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-13 10:00:51 +0330

    The django_filter is added.

--- backend/Server/Server/settings.py
+++ backend/Server/Server/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     # Third party libs
     'ckeditor',
     'rest_framework',
+    'django_filters',
     'django_cleanup.apps.CleanupConfig',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
@@ -168,6 +169,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 
