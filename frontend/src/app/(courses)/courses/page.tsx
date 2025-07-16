80b9f765f93dd4a078e08558efe285978f220105
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-17 01:20:18 +0330

    The CoursesPage is updated and the CoursesFilter component added to this page.

--- frontend/src/app/(courses)/courses/page.tsx
+++ frontend/src/app/(courses)/courses/page.tsx
@@ -2,19 +2,18 @@ import { Fragment } from "react";
 
 import { getServerSession } from "next-auth/next";
 
-import GradientCourseCard from "@/components/courses/GradientCourseCard";
-
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-
-import Course from "@/types/Course";
-
-import HeroSection from "@/components/HeroSection";
 import { MdOutlineFeaturedPlayList } from 'react-icons/md';
 
+import CoursesFilter from "@/components/courses/CoursesFilter";
+import HeroSection from "@/components/HeroSection";
+import GradientCourseCard from "@/components/courses/GradientCourseCard";
+import Course from "@/types/course"
 
 
 
 export default async function CoursesPage() {
+
   const session = await getServerSession(authOptions);
 
   if (!session) {
@@ -31,7 +30,7 @@ export default async function CoursesPage() {
   const courses: Course[] = await response.json();
 
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="flex justify-evenly items-center flex-col gap-12 overflow-hidden">
       <div className="max-w-7xl w-full">
         <HeroSection
           title="دوره هاس مجموعه"
@@ -43,12 +42,17 @@ export default async function CoursesPage() {
           buttonUrl="/courses"
         />
       </div>
-      <div className="grid gap-12 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
-        {courses.map(course => (
-          <Fragment key={course.slug}>
-            <GradientCourseCard {...course} />
-          </Fragment>
-        ))}
+      <div className="grid grid-cols-12 items-start">
+        <div className="flex justify-center items-center bg-base-light dark:bg-base-dark border-primary-light/[0.5] dark:border-primary-dark/[0.5] border-2 p-4 rounded-xl max-w-[20vw] xl:col-span-3 lg:col-span-3">
+          <CoursesFilter />
+        </div>
+        <div className="grid gap-12 xl:grid-cols-3 lg:grid-cols-2 xl:col-span-9 lg:col-span-9">
+          {courses.map(course => (
+            <Fragment key={course.slug}>
+              <GradientCourseCard {...course} />
+            </Fragment>
+          ))}
+        </div>
       </div>
     </main>
   );
