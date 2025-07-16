99a475f2d2db1f9dcb0682b37245ef8ce694c1d7
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-16 12:28:56 +0330

    The courseFiltersData is added to the index.ts file in data folder.

---
+++ frontend/src/data/index.ts
@@ -0,0 +1,82 @@
+export const courseFiltersData = {
+    courseStatus: [
+        {
+            name: "تکمیل شده",
+            value: "C"
+        },
+        {
+            name: "درحال برگزاری",
+            value: "I"
+        },
+        {
+            name: "شروع به زودی",
+            value: "S"
+        }
+    ],
+    levelStatus: [
+        {
+            name: "مقدماتی",
+            value: "IN"
+        },
+        {
+            name: "متوسط",
+            value: "IM"
+        },
+        {
+            name: "پیشرفته",
+            value: "AD"
+        },
+        {
+            name: "مقدماتی تا پیشرفته",
+            value: "IA"
+        }
+    ],
+    paymentStatus: [
+        {
+            name: "پولی",
+            value: "P"
+        },
+        {
+            name: "رایگان",
+            value: "F"
+        }
+    ],
+    languageStatus: [
+        {
+            name: "فارسی",
+            value: "FA"
+        },
+        {
+            name: "انگلیسی",
+            value: "EN"
+        }
+    ],
+    ordering: [
+        {
+            name: "کمترین قیمت",
+            value: "price"
+        },
+        {
+            name: "بیشترین قیمت",
+            value: "-price"
+        },
+
+        {
+            name: "پربازدید ترین",
+            value: "views"
+        },
+        {
+            name: "کم بازدید ترین",
+            value: "-views"
+        },
+
+        {
+            name: "جدید تیرن",
+            value: "published_date"
+        },
+        {
+            name: "قدیمی ترین",
+            value: "-published_date"
+        },
+    ]
+}
