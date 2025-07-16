a21dda187fedc3e9b8cf6353d477629df567ece7
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-16 23:28:52 +0330

    The main-category type file is added.

---
+++ frontend/src/types/main-category.ts
@@ -0,0 +1,10 @@
+interface MainCategory {
+  title: string;
+  slug: string;
+  icon: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export default MainCategory;
