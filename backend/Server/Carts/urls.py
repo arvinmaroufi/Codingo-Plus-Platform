1e3de39345e5e8cb8c5afdb0f674f84e8610faf1
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-19 08:23:00 +0430

    CartRouter is added to urls.py file.

--- backend/Server/Carts/urls.py
+++ backend/Server/Carts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .router import CouponRouter
+from .router import CouponRouter, CartRouter
 
 
 app_name = 'Carts'
 
 
 coupon_router = CouponRouter()
+cart_router = CartRouter()
 
 urlpatterns = [
     path('coupons/', include(coupon_router.get_urls())),
