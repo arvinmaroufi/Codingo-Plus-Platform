8cada18578e33a12865f9aedae016dcaf572b525
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-19 07:58:17 +0430

    IsCouponOwnerOrAdmin is added in permissions.py file.

--- backend/Server/Carts/permissions.py
+++ backend/Server/Carts/permissions.py
@@ -6,3 +6,10 @@ class IsCartOwnerOrAdmin(BasePermission):
         if request.user.is_staff:
             return True
         return obj.user == request.user
+
+
+class IsCouponOwnerOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        return obj.user == request.user if obj.user else False
