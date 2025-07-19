30d75304b2693ab40ba2941a32aae6833d89b38e
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-19 08:14:01 +0430

    CouponViewSet is added to views.py file.

--- backend/Server/Carts/views.py
+++ backend/Server/Carts/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.viewsets import ViewSet
+from rest_framework.views import Response, APIView
+from rest_framework import status
+
+from .models import Coupon
+from .serializers import CouponSerializer
+from .permissions import IsCouponOwnerOrAdmin
+
+
+
+class CouponViewSet(ViewSet):
+    
+    permission_classes = [IsCouponOwnerOrAdmin]
+    
+    def list(self, request):
+        if request.user.is_staff:
+            queryset = Coupon.objects.all()
+        else:
+            queryset = Coupon.objects.filter(user=request.user) | Coupon.objects.filter(user__isnull=True)
+        serializer = CouponSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        coupon = get_object_or_404(Coupon, pk=pk)
+        self.check_object_permissions(request, coupon)
+        serializer = CouponSerializer(coupon)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = CouponSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'The Coupon is added.'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        coupon = get_object_or_404(Coupon, pk=pk)
+        self.check_object_permissions(request, coupon)
+        serializer = CouponSerializer(coupon, data=request.data, partial=True, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'The Coupon is updated.'}, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        coupon = get_object_or_404(Coupon, pk=pk)
+        self.check_object_permissions(request, coupon)
+        coupon.delete()
+        return Response({'message': 'The Coupon is deleted.'}, status=status.HTTP_204_NO_CONTENT)
