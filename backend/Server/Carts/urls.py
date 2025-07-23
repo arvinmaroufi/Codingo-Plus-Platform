e91fb059673e512e2340c87233b9fae57d58a022
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-23 15:44:33 +0330

    The Cart routers and urls optimized.

--- backend/Server/Carts/urls.py
+++ backend/Server/Carts/urls.py
@@ -10,6 +10,8 @@ cart_router = CartRouter()
 course_item_router = CourseItemRouter()
 
 urlpatterns = [
+    path('carts/', include(cart_router.get_urls())),
     path('coupons/', include(coupon_router.get_urls())),
+    path('course-items/', include(course_item_router.get_urls())),
 ]
 
