ae55ea26f79c639dd7e04ad48cec1abcc1ac6b3b
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-24 12:57:46 +0430

    OrderRouter is added to urls.py file.

--- backend/Server/Orders/urls.py
+++ backend/Server/Orders/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from .router import OrderRouter
 
 
 app_name = 'Orders'
 
+
+order_router = OrderRouter()
+
 urlpatterns = [
-    
+    path('orders/', include(order_router.get_urls())),
+
 ]
