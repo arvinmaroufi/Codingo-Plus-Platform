10080306771ff3aaf06f267a77ed0b4001299248
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-25 04:15:34 +0430

    WalletRouter is added to urls.py file.

--- backend/Server/Wallets/urls.py
+++ backend/Server/Wallets/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from .router import WalletRouter
 
 
 app_name = 'Wallets'
 
+wallet_router = WalletRouter()
 
 urlpatterns = [
-    
+    path('wallets/', include(wallet_router.get_urls())),
 ]
