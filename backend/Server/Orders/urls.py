ba491b2e479ce6e3ad98d5c9a8306ede5c7c7c5a
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-24 12:58:18 +0430

    OrderCourseItemRouter is added to urls.py file.

--- backend/Server/Orders/urls.py
+++ backend/Server/Orders/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
-from .router import OrderRouter
+from .router import OrderRouter, OrderCourseItemRouter
 
 
 app_name = 'Orders'
 
 
 order_router = OrderRouter()
+order_item_router = OrderCourseItemRouter()
 
 urlpatterns = [
     path('orders/', include(order_router.get_urls())),
-
+    path('order-items/', include(order_item_router.get_urls())),
 ]
