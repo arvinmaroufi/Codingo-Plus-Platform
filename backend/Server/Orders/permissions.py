87a1aad2e29157b8556606d8341dd0e8d84ad131
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-07-24 12:05:59 +0430

    IsOrderItemOwnerOrAdmin is added in permissions.py file.

--- backend/Server/Orders/permissions.py
+++ backend/Server/Orders/permissions.py
@@ -6,3 +6,10 @@ class IsOrderOwnerOrAdmin(BasePermission):
         if request.user.is_staff:
             return True
         return obj.user == request.user
+
+
+class IsOrderItemOwnerOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        return obj.order.user == request.user
