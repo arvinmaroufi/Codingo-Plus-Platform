e0c4a41b92c661dae838c34367c043a34ac59cdb
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-29 09:51:49 +0330

    The AdminProfileRouter and urls are added for admin profiles paths.

---
+++ backend/Server/Profiles/routers.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+
+
+
+class AdminProfileRouter(routers.DefaultRouter):
+    def __init__(self):
+        super().__init__()
+        self.register(r'', views.AdminProfileViewSet, basename='admin-profiles')
+
+    def get_urls(self):
+        return [
+            path('', include([
+                path('', views.AdminProfileViewSet.as_view({'get': 'list'})),
+                path('<str:username>/', views.AdminProfileViewSet.as_view({'get': 'retrieve', 'put': 'update'})),
+            ])),
+        ]
+    
--- backend/Server/Profiles/urls.py
+++ backend/Server/Profiles/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from . import routers
 
 
 app_name = "Profiles"
 
 
-urlpatterns = [
 
+admin_profile_router = routers.AdminProfileRouter()
+
+
+
+urlpatterns = [
+    path('admin/', include(admin_profile_router.get_urls())),
 ]
