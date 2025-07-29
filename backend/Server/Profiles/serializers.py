78931125c90ab592b77c737e3b1460b9a7d5b3af
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-29 10:11:42 +0330

    The StudentProfileSerializer is added with the update method.

--- backend/Server/Profiles/serializers.py
+++ backend/Server/Profiles/serializers.py
@@ -21,6 +21,29 @@ class AdminProfileSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         instance.age = validated_data.get('age', instance.age)
         instance.bio = validated_data.get('bio', instance.bio)
+        instance.gender = validated_data.get('gender', instance.gender)
+        instance.profile_picture = validated_data.get('profile_picture', instance.profile_picture)
+        instance.is_available = validated_data.get('is_available', instance.is_available)
+        instance.profile_banner = validated_data.get('profile_banner', instance.profile_banner)
+
+        instance.save()
+        return instance
+    
+
+
+class StudentProfileSerializer(serializers.ModelSerializer):
+
+    user = serializers.SlugRelatedField(slug_field="username", read_only=True)
+
+    class Meta:
+        model = StudentProfile
+        fields = "__all__"
+
+
+    def update(self, instance, validated_data):
+        instance.age = validated_data.get('age', instance.age)
+        instance.bio = validated_data.get('bio', instance.bio)
+        instance.gender = validated_data.get('gender', instance.gender)
         instance.profile_picture = validated_data.get('profile_picture', instance.profile_picture)
         instance.is_available = validated_data.get('is_available', instance.is_available)
         instance.profile_banner = validated_data.get('profile_banner', instance.profile_banner)
