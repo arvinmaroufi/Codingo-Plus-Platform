fb599e60308c038d907eebb79c024aebdc70a878
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-07-29 11:45:40 +0330

    The Profiles routers and urls are added.

--- backend/Server/Profiles/urls.py
+++ backend/Server/Profiles/urls.py
@@ -7,9 +7,15 @@ app_name = "Profiles"
 
 
 admin_profile_router = routers.AdminProfileRouter()
+student_profile_router = routers.StudentProfileViewSet()
+teacher_profile_router = routers.TheacherProfileViewSet()
+supporter_profile_router = routers.SupporterProfileViewSet()
 
 
 
 urlpatterns = [
     path('admin/', include(admin_profile_router.get_urls())),
+    path('student/', include(student_profile_router.get_urls())),
+    path('teacher/', include(teacher_profile_router.get_urls())),
+    path('supporter/', include(supporter_profile_router.get_urls())),
 ]
